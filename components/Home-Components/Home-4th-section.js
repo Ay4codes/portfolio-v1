61cd12f3c525cd0d26d906598d6c0bafcd4361fc
Ayomide <isholaolawale256@gmail.com>
2022-12-26 23:35:48 +0100

    4th Section completion

--- components/Home-Components/Home-4th-section.js
+++ components/Home-Components/Home-4th-section.js
@@ -5,7 +5,8 @@ export default function Home4thSection() {
     return (
         <div className="home-4th-section-wrapper">
             <div className="home-4th-section-inner">
-                <h2>Let's skyrocket your next project together.</h2>
+                <div className="section-title-btn-icon section-title-btn-icon2 rounded-full"></div>
+                <h2>Let's skyrocket your <br /> next project together.</h2>
                 <Link href={'/contact'}>
                     <button>Contact me</button>
                 </Link>
