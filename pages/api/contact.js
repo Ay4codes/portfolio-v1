2faac1f8200064e0398bb9d4fcf5a79a7411f086
Ayomide <isholaolawale256@gmail.com>
2023-01-04 10:40:46 +0100

    Node Mailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,40 +1,37 @@
 import nodemailer from 'nodemailer'
 
 export default async function contact(req,res) {
-    const {name, email, company, subject, message} = req.body
+    try {
+        const {name, email, company, subject, message} = req.body
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        secure: false,
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.APP_PASSWORD
-        }
-    });
-
-    function verifyConnection() {
-        transporter.verify(function(err) {
-            if (err) {
-                return false
-            } else {
-                return true
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            secure: false,
+            auth: {
+              user: process.env.EMAIL,
+              pass: process.env.APP_PASSWORD
             }
-        })
-    }
+        });
 
-    if (verifyConnection()) {
+        function verifyConnection() {
+            transporter.verify(function(err) {
+                if (err) {
+                    return false
+                } else {
+                    return true
+                }
+            })
+        }
         const mailOptions = {
             from: process.env.EMAIL,
             to: process.env.EMAIL,
             subject: 'Message From My Portfolio',
             html: `
                     <h3>Message From My Portfolio</h3>
-                    <br />
                     <p>Name: <b>${name}</b></p>
                     <p>Email: <b>${email}</b></p>
                     <p>Subject: <b>${subject}</b></p>
                     <p>Company: <b>${company}</b></p>
-                    <br />
                     <br />
                     <p>Message: <b>${message}</b></p>
                 `
@@ -47,7 +44,7 @@ export default async function contact(req,res) {
                 res.status(200).send('')
             }
         });
-    } else {
-        res.status(500).send('')
+    } catch (error) {
+        console.log(error);
     }
 }
