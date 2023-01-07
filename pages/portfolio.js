aa5fdc34465612e230b608b5f5c1b6435ee79da4
Ayomide <isholaolawale256@gmail.com>
2023-01-07 11:08:17 +0100

    Portfolio Page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,8 +1,20 @@
+import Portfolio from "../components/Portfolio-components/Portfolio"
+import Head from 'next/head'
+import { useEffect } from 'react';
+import NavBar from '../components/Global-components/Navbar';
+import Footer from '../components/Global-components/Footer';
 
-export default function Portfolio() {
+export default function PortfolioPage(props) {
+    const {pageVisits} = props
+
     return (
-        <div>
-            Porfolio
+        <div className={'app-main-wrapper'}>
+            <Head>
+              <title>Portfolio - Ayomide Ishola || @ay4codes</title>
+            </Head>
+            <NavBar />
+            <Portfolio />
+            <Footer pageVisits={pageVisits} />
         </div>
     )
 }
