a29d7feaf3e00e2ccdada59e1ef7b1cfea1ecc35
Ayomide <isholaolawale256@gmail.com>
2023-01-10 13:57:41 +0100

    Portfolio Items

--- components/Global-components/Portfolio-item.js
+++ components/Global-components/Portfolio-item.js
@@ -1,11 +1,10 @@
 import Link from "next/link"
-import Image from "next/image"
 
 export default function Project(props) {
     return (
         <div className="project-wrapper">
             <div className="project-img-wrapper">
-                <Image width={600} height={370} src={props.img} alt={props.alt} />
+                <img src={props.img} alt={props.alt} />
             </div>
             <div className="project-content-wrapper">
                 <button className="project-title-btn">{props.category}</button>
