73eefaedb17a9dcbce13e91dfa7efa80f1cc0d1c
Ayomide <isholaolawale256@gmail.com>
2023-01-24 19:45:54 +0100

    Added my feedbacks from Feedbackup

--- components/Home-Components/Home-5th-section.js
+++ components/Home-Components/Home-5th-section.js
@@ -7,11 +7,11 @@ export default function Home5thSection() {
             <SectionTitle title={"Don't take my words for it."} />
             <div className="home-5th-section-inner">
                 <div className="home-5th-content-wrapper">
-                    <iframe src="https://feedbackup.netlify.app/feedback/639fb98dc0af08cbf8054a4e" width={"350px"} height={"240px"} style={{borderRadius: '15px', boxShadow: "0 0 5px 0 rgba(39, 25, 77, 0.02), 0 3px 20px 0 rgba(112, 81, 239, 0.04), 0 10px 28px 0 rgba(39, 25, 77, 0.03)"}} frameBorder="0" marginHeight="0" marginWidth="0" />
+                <iframe src="https://feedbackup.vercel.app/feedback/63d02425d9da11701fc259e9" width={"350px"} height={"220px"} style={{borderRadius: '15px', boxShadow: "0px 1px 12px 6px rgba(234, 76, 137, 0.21)"}} frameborder="0" marginheight="0" marginwidth="0" />
                 </div>
                 <div className="home-5th-content-wrapper home-5th-content-wrapper2">
-                    <iframe src="https://feedbackup.netlify.app/feedback/639fb98dc0af08cbf8054a4e" width={"350px"} height={"240px"} style={{borderRadius: '15px', boxShadow: "0 0 5px 0 rgba(39, 25, 77, 0.02), 0 3px 20px 0 rgba(112, 81, 239, 0.04), 0 10px 28px 0 rgba(39, 25, 77, 0.03)"}} frameBorder="0" marginHeight="0" marginWidth="0" />
-                    <iframe className="ifr-nd" src="https://feedbackup.netlify.app/feedback/639fb98dc0af08cbf8054a4e" width={"350px"} height={"240px"} style={{borderRadius: '15px', boxShadow: "0 0 5px 0 rgba(39, 25, 77, 0.02), 0 3px 20px 0 rgba(112, 81, 239, 0.04), 0 10px 28px 0 rgba(39, 25, 77, 0.03)"}} frameBorder="0" marginHeight="0" marginWidth="0" />
+                    <iframe src="https://feedbackup.vercel.app/feedback/63d02518b984828c2cfaca0e" width={"350px"} height={"220px"} style={{borderRadius: '15px', boxShadow: "0px 1px 12px 6px rgba(234, 76, 137, 0.21)"}} frameborder="0" marginheight="0" marginwidth="0" />
+                    <iframe src="https://feedbackup.vercel.app/feedback/63d0268ab984828c2cfad113" width={"350px"} height={"220px"} style={{borderRadius: '15px', boxShadow: "0px 1px 12px 6px rgba(234, 76, 137, 0.21)"}} frameborder="0" marginheight="0" marginwidth="0" />
                 </div>
             </div>
         </div>
