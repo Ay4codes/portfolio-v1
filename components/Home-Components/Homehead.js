94c593aa0548558ddbdee92bcbce22afa7d14b99
Ishola Ayomide <101745279+Ay4codes@users.noreply.github.com>
2023-09-25 19:33:58 +0100

    Updated resume

--- components/Home-Components/Homehead.js
+++ components/Home-Components/Homehead.js
@@ -26,7 +26,7 @@ export default function Homehead() {
                             <Link href={'/contact'}>
                                 <div className="contact-me-btn">Contact Me</div>
                             </Link>
-                            <Link target="_blank" rel="noopener noreferrer" href={'https://drive.google.com/file/d/1onqpX5forLS-_oqlffZkgklJu40pHqyY/view?usp=share_link'}>
+                            <Link target="_blank" rel="noopener noreferrer" href={'https://drive.google.com/file/d/18iTirNRuwqf6aH_cRQdXl3hg3B0_0Fzq/view?usp=drive_link'}>
                                 <div className="browse-cv-btn">Browse CV</div>
                             </Link>
                         </div>
